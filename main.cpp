38f2c9aa5297baad44e3f1979e12b608d7966ecb
ahbailar <115739286+ahbailar@users.noreply.github.com>
2022-12-02 10:39:30 -0600

    code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 using namespace std;
 int main(){
+  int n, num;
+  double sum;
+  
+  cout << "How many numbers are you going to type? ";
+  cin >> n;
+  
+  for(int counter = 0; counter < n; counter++){
+    cout << c << " ";
+  }
+  cout << endl;
 
-
-
-return 0;
+  return 0;
 }
